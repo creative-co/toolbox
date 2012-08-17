680f3f16603c83acdf6b8617d188bed63de881e8
ilyav <strikeroff@gmail.com>
2012-08-17 18:42:48 +0400

    Update jss_template/railtie.rb
    run jss template initialization after "sprockets.environment" for jss_tempaltes

--- jss_template/railtie.rb
+++ jss_template/railtie.rb
@@ -3,7 +3,7 @@ require 'rails/railtie'
 module Jss
   module Rails
     class Railtie < ::Rails::Railtie
-      config.before_initialize do
+      initializer "jss_template", :after => "sprockets.environment" do
         Sprockets::Engines # force loading
         Sprockets.register_engine '.jss', Jss::Template
       end
